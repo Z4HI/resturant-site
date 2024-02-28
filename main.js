5cfd96f2eee162c44b9819c95169937fd87c4138
Zahi <102051293+Z4HI@users.noreply.github.com>
2024-02-28 08:53:57 -0500

    deploy

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 import './style.css'
 const preloader = document.querySelector('.preloader')
+const app = document.querySelector('.app')
+const home = document.querySelector('.home')
+const menu = document.querySelector('.menu')
+const about = document.querySelector('.about')
+const contact = document.querySelector('.contact')
+const reviews = document.querySelector('.reviews')
 
 window.onload = function(){
     preloader.classList.add('loaded')
+    homePage()
 }
+
+home.addEventListener('click', ()=>{
+
+homePage()
+
+})
+
+menu.addEventListener('click', ()=>{
+
+app.innerHTML = ""
+
+})
+
+
+
+
+/* const homePage = ()=>{
+const feature = document.createElement('div')
+feature.classList.add('feature')
+feature.innerHTML = 
+        `<h3>"NY Times best Steak of 2024"</h3>`+
+          `<button class="reserveBtn">Reservations</button>`
+
+app.appendChild(feature)
+
+}*/
